699914591557943fa5dc8085a8abfef84996f167
jihadrekani <jihadfed95@gmail.com>
2022-11-11 21:00:29 +0300

    add blog  , ingredients and  pride section

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+import blog1 from './images/1.jpg'
+import blog2 from './images/2.jpg'
+import blog3 from './images/3.jpg'
+
+
+const Data = [
+    {
+        img: blog1,
+        title: 'Rainbow Vegetable Sandwich',
+        time: 'Time: 15 - 20 Minutes ',
+        price: '$10.50'
+    }
+,
+    {
+        img: blog2,
+        title: 'Vegetarian Burger',
+        time: 'Time: 30 - 45 Minutes ',
+        price: '$9.20'
+    }
+,
+    {
+        img: blog3,
+        title: 'Raspberry Stuffed French Toast',
+        time: 'Time: 10 - 15 Minutes ',
+        price: '$12.50'
+    }
+
+
+]
+export default Data
