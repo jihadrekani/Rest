2aa5a072de87fad0b9a3744ac9458324fb9787be
jihadrekani <jihadfed95@gmail.com>
2022-11-17 11:47:23 +0300

    changed some styles

--- src/comp/Home/Home.js
+++ src/comp/Home/Home.js
@@ -74,9 +74,15 @@ We pride ourselves on making real food from the best ingredients.</h2>
         <h2>We make everything by hand with the best possible ingredients.</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
         <ul>
-          <li>Etiam sed dolor ac diam volutpat.</li>
-          <li>Erat volutpat aliquet imperdiet.</li>
-          <li>purus a odio finibus bibendum.</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#a82d49" className="bi bi-check" viewBox="0 0 16 16">
+  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+</svg>Etiam sed dolor ac diam volutpat.</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#a82d49" className="bi bi-check" viewBox="0 0 16 16">
+  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+</svg>Erat volutpat aliquet imperdiet.</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#a82d49" className="bi bi-check" viewBox="0 0 16 16">
+  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+</svg>purus a odio finibus bibendum.</li>
         </ul>
         <button><a href='#'>learn more</a></button>
       </div>
