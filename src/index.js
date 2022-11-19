0984810f3de8d8a95e53498e2864a928beac1092
jihadrekani <jihadfed95@gmail.com>
2022-11-19 16:02:07 +0300

    import ReactBootstrapCarousel from "react-bootstrap-carousel"; import "bootstrap/dist/css/bootstrap.css"; import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-carousel/dist/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import ReactBootstrapCarousel from "react-bootstrap-carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
